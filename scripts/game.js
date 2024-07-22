523df09eba4be2d1e286996b8a8c632f518e6a3e
Eden Essame <eden_essame80@hotmail.com>
2024-07-22 17:36:59 +0000

    finish, maybe missed a couple tests

--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,139 @@
+/**
+ * object to store global state
+ * sets the variables to 0 and the choices buttons
+ * so the tests in game.test.js are correct
+ * later referenced for example as game.score
+ */
 let game = {
     currentGame: [],
     playerMoves: [],
     score: 0,
+    turnNumber: 0,
+    lastButton: "",
+    turnInProgress: false,
     choices: ["button1", "button2", "button3", "button4"]
 };
 
+/**
+ * set the keys in the game object to 0 or empty arrays
+ * 
+ * runs a for loop through all the elements with a class name of "circle"
+ * and if the data-listener atribute does not equal true add the event listener "click"
+ * then (e) is the event object, the click object to be referenced later
+ * so we can then use (e) to get the targets ID, so depending on which circle we clickon, the ID will be button1/button2 etc
+ * and store that in the variable: move
+ * then call the lights on function, passing it move: lightsOn(move); to iluminate the correct circle
+ * make lastButton "move" - stores "move" in lastButton
+ * push that move into game.playerMoves array
+ * then call playerTurn function
+ * then set the data-listener attribute on our circle to true
+ * 
+ * call showScore function
+ * call addTurn
+ */
+function newGame() {
+    game.currentGame = [];
+    game.playerMoves = [];
+    game.score = 0;
 
-module.exports = { game };
+    for (let circle of document.getElementsByClassName("circle")) {
+        if (circle.getAttribute("data-listener") !== "true") {
+            circle.addEventListener("click", (e) => {
+                if (game.currentGame.length > 0 && !game.turnInProgress) {// only accept a click if currentGame length is greater than 0 and not game.turnInProgress
+                    let move = e.target.getAttribute("id");
+                    game.lastButton = move;
+                    game.playerMoves.push(move);
+                    lightsOn(move);
+                    playerTurn();
+                }
+            });
+            circle.setAttribute("data-listener", "true");
+        }
+    }
+    showScore();
+    addTurn();
+}
+
+/**
+ * clear playerMoves array
+ * randomly add a button ID to currentGame array, push that into the computer sequence array
+ * [(Math.floor(Math.random() * 4))] generates a random number between 1 and 3 to add to select from the choices array
+ * the resulting choice is pushed onto the currentGame array
+ * call showTurns function
+ */
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    showTurns();
+}
+
+/**
+ * call lightsOn function with the ID of one of the circles
+ * so this is refered to in the function as "circ"
+ * get the element
+ * add "light" class
+ * set the class to be removed after 400 milliseconds
+ * 
+ */
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(function () {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+}
+
+/**
+ * set game.turnInProgress = true; so cant click while the computer is playing
+ * set turnNumber to 0
+ * use that as the array index number for currentGame array
+ * call lightsOn function inside a JS set interval
+ * which makes sure we have a pause between the lights being shown and the next step in the sequence
+ * then incrementing the turnNumber
+ * then an if statement that if our turnNumber is equal or over the length of currentGame array, 
+ * then the sequence is complete and to clear the interval 
+ * then set game.turnInProgress = false; so the user can play
+ * 800 is the interval time
+ */
+function showTurns() {
+    game.turnInProgress = true;
+    game.turnNumber = 0;
+    let turns = setInterval(function () {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+            game.turnInProgress = false;
+        }
+    }, 800);
+}
+
+/**
+ * gets the last element from playerMoves array- let i = game.playerMoves.length - 1;
+ * so we can compare that with the same index in currentGame array
+ * if the player gets the answer correct, these two should match
+ * if they do increment gameScore
+ * call showScore and addTurn functions
+ * else send a warning alert and call newGame function
+ */
+function playerTurn() {
+    let i = game.playerMoves.length - 1;
+    if (game.currentGame[i] === game.playerMoves[i]) {
+        if (game.currentGame.length === game.playerMoves.length) {
+            game.score++;
+            showScore();
+            addTurn();
+        }
+    } else {
+        alert("Wrong move!");
+        newGame();
+    }
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+/**
+ * export the functions to import them in game.test.js
+ */
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn };
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,8 +1,21 @@
+
 /**
  * @jest-environment jsdom
  */
 
-const { game } = require("../game");
+/**
+ * import the functions from game.js to test
+ * they must be exported from game.js
+ */
+const { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn } = require("../game");
+
+/**
+ * send an alert if anything interesting happens
+ * so spy on the window and then send an "alert"
+ * alert is a method of the window object
+ * so we are going to catch it and divert it harmlessly into an empty function
+ */
+jest.spyOn(window, "alert").mockImplementation(() => { });
 
 /**
  * loads index.html into jesta mock dom
@@ -19,6 +32,11 @@ beforeAll(() => {
     document.close();
 });
 
+/**
+ * test these corresponding keys exist in game.js
+ * they are in the object- let game {score: 0, currentGame[], etc} 
+ * they are then referenced later as game.score game.currentGame
+ */
 describe("game object contains correct keys", () => {
     test("score key exists", () => {
         expect("score" in game).toBe(true);
@@ -34,5 +52,105 @@ describe("game object contains correct keys", () => {
     });
     test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+    test("turnNumber key exists", () => {
+        expect("turnNumber" in game).toBe(true);
+    });
+    test("lastButton key exists", () => {
+        expect("lastButton" in game).toBe(true);
+    });
+    test("turnInProgress key exists", () => {
+        expect("turnInProgress" in game).toBe(true);
+    });
+    test("turnInProgress key value is false", () => {
+        expect("turnInProgress" in game).toBe(true);
+    });
+});
+
+/**
+ * beforeall function: set up the game state with some fake values to see if the newgame function resets them
+ * set the score in the DOM to 42 beforeAll to test it gets reset by newGame
+ * tests the questions against the variables in game.js
+ * 
+ * gets the elements with the class name: circle, stores them in the variable: elements
+ * a for loop gets each "element" in "elements" and expects them to have the data-listener atribute set to true
+ */
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.playerMoves = ["button1", "button2"];
+        game.currentGame = ["button1", "button2"];
+        document.getElementById("score").innerText = "42";
+        newGame();
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should display 0 for the element with id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
+    });
+    test("should clear the player moves array", () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+    test("should add one move to the computer's game array", () => {
+        expect(game.currentGame.length).toBe(1);
+    });
+    test("expect data-listener to be true", () => {
+        newGame();
+        const elements = document.getElementsByClassName("circle");
+        for (let element of elements) {
+            expect(element.getAttribute("data-listener")).toEqual("true");
+        }
+    });
+});
+
+/**
+ * beforeAll runs before all tests are run
+ * beforeEach runs before each test is run
+ * afterEach resets the game state after the test, so tests can be run in any order
+ * beforeeach turn sets the score to 0 
+ * and emptys the currentGame and playerMoves arrays 
+ * calls addTurn function
+ * and does the same after
+ * tests addTurn adds a new turn: expect(game.currentGame.length).toBe(2);
+ * tests it lights up the element
+ * test showTurns function
+ */
+describe("gameplay works correctly", () => {
+    beforeEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+        addTurn();
+    });
+    afterEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+    });
+    test("addTurn adds a new turn to the game", () => {
+        addTurn();
+        expect(game.currentGame.length).toBe(2);
+    });
+    test("should add correct class to light up the buttons", () => {
+        let button = document.getElementById(game.currentGame[0]);
+        lightsOn(game.currentGame[0]);
+        expect(button.classList).toContain("light");
+    });
+    test("showTurns should update game.turnNumber", () => {
+        game.turnNumber = 42;
+        showTurns();
+        expect(game.turnNumber).toBe(0);
+    });
+    test("should increment the score if the turn is correct", () => {
+        game.playerMoves.push(game.currentGame[0]); // push the currentGame move int the playerMoves array
+        playerTurn();
+        expect(game.score).toBe(1); // after calling playerTurn we expect the score to have increased, it should start at 0
+    });
+    test("clicking during computer sequence should fail", () => {
+        showTurns();
+        game.lastButton = "";
+        document.getElementById("button2").click();
+        expect(game.lastButton).toEqual("");
     });
 });
